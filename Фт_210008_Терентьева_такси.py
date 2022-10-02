d3423711a538fd3c25f732669f4148d4607ecc27
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-10-02 20:00:51 +0500

    Update Фт_210008_Терентьева_такси.py
    Создаем массив из сотрудников и вводим расстояние в километрах до дома каждого сотрудника, а также тарифы каждого такси

--- Фт_210008_Терентьева_такси.py
+++ Фт_210008_Терентьева_такси.py
@@ -9,3 +9,18 @@ class taksi:
     def __init__(self, num, t):
         self.num=num
         self.tarif = t
+n=int(input("Введите количество сотрудников от 1 до 1000: \n"))
+rab=[]
+tak = []
+sum = 0
+l=0
+k=0
+b=100000
+i=0
+c=1
+for j in range(n):
+    kil = int(input(f"Введите расстояние в километрах для {j+1} работника: "))
+    rab.append(rabotnik((j+1), kil))
+for j in range(n):
+    tar= int(input(f"Введите тариф для {j+1} такси за километр: "))
+    tak.append(taksi((j+1), tar))
